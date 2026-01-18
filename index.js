a7a60bb85a7463f5ede0265d1cf62681a83bdf03
ARAman7860 <khanbab7865431@gmail.com>
2026-01-18 18:50:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+fb-appstate-user-bot/
+│
+├─ index.js              ← main entry file
+├─ package.json          ← dependencies info
+├─ appstate.json         ← (private data – empty ya ignored)
+├─ .gitignore            ← private files hide karne ke liye
+│
+├─ commands/             ← saare commands
+│   ├─ ping.js
+│   └─ help.js
+│
+├─ config/
+│   └─ config.json       ← bot settings
+│
+└─ README.md             ← project info
